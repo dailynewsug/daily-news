c4b06f18606a4bb6cdbb91b95727b67c526dae88
vietiritz bernard <dbernardinvestments@gmail.com>
2026-05-18 11:26:39 +0300

    Add Monetag ads

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11022344
+    "zoneId": 11022413
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
