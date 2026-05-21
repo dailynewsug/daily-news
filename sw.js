197ce8615fd626fb382ed05cc61e1c2a33d66c3b
vietiritz bernard <dbernardinvestments@gmail.com>
2026-05-21 06:59:01 +0300

    Fix service worker 404 error

---
+++ sw.js
@@ -0,0 +1,12 @@
+// Daily News Service Worker
+self.addEventListener('install', function(event) {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(fetch(event.request));
+});
